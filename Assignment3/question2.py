ee550b1a070734a92b9d5fc9df9c4a10fe6ffa9c
RituparnaAdha <rituparna1602@gmail.com>
2021-05-12 02:15:17 +0530

    adding question2

---
+++ Assignment3/question2.py
@@ -0,0 +1,41 @@
+import wandb
+from question1 import begin
+
+if __name__ == '__main__':
+    sweep_config = {
+        'method': 'bayes',
+        'metric':{
+            'goal': 'maximize',
+            'name': 'val_accuracy'
+        },
+        'parameters': {
+        'epoch': {
+            'values': [50,80]
+        },
+        'hidden_layer_size':{
+            'values': [128,256]
+        },
+        'celltype':{
+            'values': ['rnn','lstm','gru']
+        },
+        'embedding_size':{
+            'values': [16,32,64]
+        },
+        'num_encoder':{
+            'values': [1,2,3,4]
+        },
+        'num_decoder':{
+            'values': [1,2,3,4]
+        },
+        'beam_size':{
+            'values':[1,3]
+        },
+        'dropout':{
+            'values':[0.2,0.3]
+        },
+        
+    }
+    }
+    sweep_id = wandb.sweep(sweep_config,entity="assignment3",project='assignment3-part1')
+    wandb.agent(sweep_id, function=begin)
+    wandb.finish()
